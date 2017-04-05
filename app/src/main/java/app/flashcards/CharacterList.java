e75674d6f30238ef6c28d929a06f3f6746ad4cf4
tim <tlee9383@gmail.com>
2017-04-04 23:02:13 -0700

    why is it so hard to read a text file on android????

--- app/src/main/java/app/flashcards/CharacterList.java
+++ app/src/main/java/app/flashcards/CharacterList.java
@@ -2,42 +2,41 @@ package app.flashcards;
 
 import android.content.Context;
 import android.content.res.AssetManager;
-import android.widget.TextView;
-
-import org.json.*;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class CharacterList {
-    private Context mcontext;
     protected ArrayList<Character> chlist;
+    String a = null;
+    Context mcontext;
 
     public CharacterList(Context c){
-        mcontext = c;
         chlist = new ArrayList<Character>();
+        mcontext = c;
         //read in the data
-        //readData();
+        readData();
     }
 
-    public String readData(){
+    public void readData() {
         AssetManager am = mcontext.getAssets();
-        String s = null;
+        StringBuilder text = new StringBuilder();
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(am.open("ch.json")));
-            while(!br.ready());
-            s= br.toString();
-            //JSONTokener jst = new JSONTokener(br.toString());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            File f = new File("ch.json");
+            BufferedReader br = new BufferedReader(new InputStreamReader(am.open("example.csv")));
+            String line;
+            while ((line = br.readLine()) != null){
+                text.append(line);
+                text.append('\n');
+            }
+            br.close();
+            a = br.toString();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return s;
     }
 
 }
