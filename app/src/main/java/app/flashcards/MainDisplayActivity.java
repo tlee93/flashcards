37200136233aba302b8075e02676db8c5fad986f
tim <tlee9383@gmail.com>
2017-05-14 17:42:40 -0700

    added splash screen and cleaned up ui

--- app/src/main/java/app/flashcards/MainDisplayActivity.java
+++ app/src/main/java/app/flashcards/MainDisplayActivity.java
@@ -1,6 +1,7 @@
 package app.flashcards;
 
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,10 +19,9 @@ public class MainDisplayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loading_screen);
-        ArrayList<Character> characterList = CharacterList.getCharacterList();
-
         setContentView(R.layout.activity_main_display);
+
+        ArrayList<Character> characterList = CharacterList.getCharacterList();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -31,8 +31,13 @@ public class MainDisplayActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         CharacterListAdapter characterListAdapter = new CharacterListAdapter(this, characterList);
         recyclerView.setAdapter(characterListAdapter);
-        recyclerView.scrollToPosition(getPosition());
         recyclerView.addOnScrollListener(new MainDisplayScrollListener());
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        recyclerView.scrollToPosition(getPosition());
     }
 
     @Override
@@ -54,23 +59,37 @@ public class MainDisplayActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        AlertDialog.Builder alertDialogBuilder;
+        AlertDialog alertDialog;
         switch (item.getItemId()) {
             case R.id.action_help:
+                alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setTitle("Help");
+                alertDialogBuilder.setMessage(R.string.help_message).setCancelable(true);
+                alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
                 return true;
             case R.id.action_info:
+                alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setTitle("About");
+                alertDialogBuilder.setMessage(R.string.info_message).setCancelable(true);
+                alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
-
         }
     }
 
     private class MainDisplayScrollListener extends RecyclerView.OnScrollListener {
         SharedPreferences.Editor editor = getSharedPreferences("currentPosition", MODE_PRIVATE).edit();
+
         @Override
         public void onScrolled(RecyclerView recyclerView, int dx, int dy){
             editor.putInt("position", layoutManager.findFirstVisibleItemPosition());
             editor.commit();
         }
     }
+
+
 }
---
+++ app/src/main/java/app/flashcards/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package app.flashcards;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainDisplayActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
